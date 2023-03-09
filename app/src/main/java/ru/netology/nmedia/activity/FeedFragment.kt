0b8720dbcdf18103f0f86fb62eb579ee7685f8d8
Konovalov Kirill <kirkonowalow@gmail.com>
2023-03-09 11:21:54 +0300

    test

--- app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -94,7 +94,6 @@ class FeedFragment : Fragment() {
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_feedFragment_to_newPostFragment)
         }
-
         return binding.root
     }
 }
